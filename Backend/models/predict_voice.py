01bf4129b0cd1f69ef1beeed0f01585ab9242c06
Vrushali-04 <vrushalirathod2331@gmail.com>
2026-02-28 13:07:49 +0530

    Commit

--- Backend/models/predict_voice.py
+++ Backend/models/predict_voice.py
@@ -37,7 +37,9 @@ def predict_voice(input_data):
         prediction = model.predict(features_scaled)[0]
         probabilities = model.predict_proba(features_scaled)[0]
 
-        confidence = round(float(np.max(probabilities)) * 100, 2)
+        print("Probabilities:", probabilities)
+
+        confidence = round(float(probabilities[1]) * 100, 2)    
 
         result = "Parkinson's Detected" if prediction == 1 else "Healthy"
 
